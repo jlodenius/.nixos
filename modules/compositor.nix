2f0192ff22ecc137df2cce740c8b507aba368bff
jacob <jacoblodenius@gmail.com>
2026-01-02 21:04:48 +0100

    Host thinkpad | Gen 62: 2026-01-02 21:04:48

--- modules/compositor.nix
+++ modules/compositor.nix
@@ -8,7 +8,6 @@
   imports = [
     ./home/compositor.nix
     ./base.nix
-    ./audio.nix
   ];
 
   config = {
@@ -23,6 +22,32 @@
       settings = {
         Policy = {
           AutoEnable = true;
+        };
+      };
+    };
+
+    # Audio
+    security.rtkit.enable = true;
+    services.pulseaudio.enable = false;
+
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+
+      wireplumber = {
+        enable = true;
+        extraConfig = {
+          "10-bluez" = {
+            "monitor.bluez.properties" = {
+              "bluez5.enable-sbc-xq" = true;
+              "bluez5.enable-msbc" = true;
+              "bluez5.enable-hw-volume" = true;
+              "bluez5.codecs" = ["sbc" "sbc_xq" "aac" "ldac" "aptx" "aptx_hd"];
+            };
+          };
         };
       };
     };
