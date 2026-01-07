12f226eb9b1fc6ad3d57f2bcf6c4df6d805cdc7d
jacob <jacoblodenius@gmail.com>
2026-01-07 19:02:55 +0100

    Host thinkpad | Gen 103: 2026-01-07 19:02:55

--- modules/dev.nix
+++ modules/dev.nix
@@ -17,6 +17,35 @@
   virtualisation.docker.enable = true;
   users.users.jacob.extraGroups = ["docker"];
 
+  # Enable nix-ld
+  programs.nix-ld.enable = true;
+
+  # Define the libraries available to unpatched binaries
+  programs.nix-ld.libraries = with pkgs; [
+    # Basic system libs
+    stdenv.cc.cc
+    zlib
+    fuse3
+    icu
+    nss
+    nspr
+    openssl
+    curl
+    expat
+
+    # Graphics/Browser libs (Required for Playwright/Chromium)
+    atk
+    at-spi2-atk
+    libdrm
+    mesa
+    libgbm
+    pango
+    cairo
+    alsa-lib
+    dbus
+    glib
+  ];
+
   environment.systemPackages = with pkgs; [
     gcc
     gnumake
