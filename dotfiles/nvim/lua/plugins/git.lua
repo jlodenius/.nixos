ac7f2a532ddb1f124dde33d16b08e7bfedb6845b
jacob <jacoblodenius@gmail.com>
2026-01-12 12:38:21 +0100

    add vgit instead of git-conflict

--- dotfiles/nvim/lua/plugins/git.lua
+++ dotfiles/nvim/lua/plugins/git.lua
@@ -6,8 +6,15 @@ return {
     config = true,
   },
   {
-    "akinsho/git-conflict.nvim",
-    version = "*",
-    config = true,
+    "tanvirtin/vgit.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons" },
+    -- Lazy loading on 'VimEnter' event is necessary.
+    event = "VimEnter",
+    keys = {
+      { "<leader>cb", "<cmd>VGit buffer_conflict_accept_both<cr>", desc = "VGit: Accept Both" },
+      { "<leader>co", "<cmd>VGit buffer_conflict_accept_current<cr>", desc = "VGit: Accept Current" },
+      { "<leader>ct", "<cmd>VGit buffer_conflict_accept_incoming<cr>", desc = "VGit: Accept Incoming" },
+    },
+    config = function() require("vgit").setup() end,
   },
 }
