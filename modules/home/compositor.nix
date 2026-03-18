e72262f3c36d7dfd01c962a7469390c7c6838f63
jacob <jacoblodenius@gmail.com>
2026-03-18 15:46:05 +0100

    Host thinkpad | Gen 167: 2026-03-18 15:46:05

--- modules/home/compositor.nix
+++ modules/home/compositor.nix
@@ -25,6 +25,18 @@
       BROWSER = "qutebrowser";
     };
 
+    # Default applications
+    xdg.mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "text/html" = "org.qutebrowser.qutebrowser.desktop";
+        "x-scheme-handler/http" = "org.qutebrowser.qutebrowser.desktop";
+        "x-scheme-handler/https" = "org.qutebrowser.qutebrowser.desktop";
+        "x-scheme-handler/about" = "org.qutebrowser.qutebrowser.desktop";
+        "x-scheme-handler/unknown" = "org.qutebrowser.qutebrowser.desktop";
+      };
+    };
+
     # Set up directories
     xdg.userDirs = {
       enable = true;
