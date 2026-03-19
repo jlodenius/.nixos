4c16bc773bd8e14b28171c71734c263f9d99df2c
jacob <jacoblodenius@gmail.com>
2026-03-19 13:08:25 +0100

    Host thinkpad | Gen 168: 2026-03-19 13:08:25

--- modules/dev.nix
+++ modules/dev.nix
@@ -4,12 +4,16 @@
     ./home/dev.nix
   ];
 
-  # SIS Dev
+  security.pki.certificateFiles = [
+    ../certs/SIS-RootCA.crt
+  ];
+
   networking.hosts = {
     "127.0.0.1" = [
       "mol-dev.sis.se"
       "mol-admin-dev.sis.se"
       "dev-viewer.standard.sis.se"
+      "sd-api.dev.sis.se"
     ];
   };
 
