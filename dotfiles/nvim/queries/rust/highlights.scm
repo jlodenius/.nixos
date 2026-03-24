a16d97db7b03c6ec56cdb463723d0b8d25bec81a
jacob <jacoblodenius@gmail.com>
2026-03-24 15:16:01 +0100

    remove old nvim dotfiles

--- dotfiles/nvim/queries/rust/highlights.scm
+++
@@ -1,8 +0,0 @@
-; extends
-
-; Distinguish tuple parentheses from regular parentheses
-(tuple_expression ["(" ")"] @constructor)
-
-; Distinguish unary operators from binary operators
-(reference_type "&" @keyword)
-(unary_expression ["*" "&"] @keyword)
