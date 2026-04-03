038b07a542784dd15ece1acd5c6abd649fed0112
jacob <jacoblodenius@gmail.com>
2026-04-03 22:56:33 +0200

    move bookmarks

--- modules/qutebrowser/qutebrowser.nix
+++ modules/qutebrowser/qutebrowser.nix
@@ -17,6 +17,34 @@
           ch = "https://www.chess.com";
         };
 
+        # Bookmarks
+        xdg.configFile."qutebrowser/bookmarks/urls".text = let
+          bookmarks = {
+            sis = [
+              ["sis/repos" "https://dev.azure.com/swedishinstituteforstandards/_git/sis-frontend"]
+              ["sis/teams" "https://teams.cloud.microsoft"]
+              ["sis/time" "https://my355752-sso.sapbydesign.com/sap/ap/ui/repository/SAP_UI/HTMLOBERON5/client.html"]
+              ["sis/jira" "https://sisswe.atlassian.net/jira/software/c/projects/SU/boards/24/backlog"]
+              ["sis/azure" "https://portal.azure.com/?feature.msaljs=true#browse/Microsoft.App%2FcontainerApps"]
+              ["sis/timereport" "https://my.kleer.se/web2/time-reporting/month"]
+              ["sis/viewer" "https://dev-viewer.standard.sis.se"]
+              ["sis/viewer/test" "https://viewer-tst.standard.sis.se"]
+              ["sis/viewer/swagger" "https://api-dev.standard.sis.se/swagger/index.html"]
+              ["sis/mol" "https://mol-dev.sis.se"]
+              ["sis/mol-admin" "https://mol-admin-dev.sis.se"]
+            ];
+            caesari = [
+              ["caesari/gh" "https://github.com/caesariab/caesari2/pulls"]
+            ];
+            other = [
+              ["sports" "http://www.fawanews.sc"]
+            ];
+          };
+          toLine = pair: "${builtins.elemAt pair 1} ${builtins.elemAt pair 0}";
+          all = builtins.concatLists (builtins.attrValues bookmarks);
+        in
+          builtins.concatStringsSep "\n" (map toLine all);
+
         keyBindings = {
           normal = {
             "<Ctrl-j>" = "completion-item-focus next";
@@ -184,34 +212,6 @@
           colors.tabs.selected.even.bg = c.selection;
         };
       };
-
-      # Bookmarks
-      xdg.configFile."qutebrowser/bookmarks/urls".text = let
-        bookmarks = {
-          sis = [
-            ["sis/repos" "https://dev.azure.com/swedishinstituteforstandards/_git/sis-frontend"]
-            ["sis/teams" "https://teams.cloud.microsoft"]
-            ["sis/time" "https://my355752-sso.sapbydesign.com/sap/ap/ui/repository/SAP_UI/HTMLOBERON5/client.html"]
-            ["sis/jira" "https://sisswe.atlassian.net/jira/software/c/projects/SU/boards/24/backlog"]
-            ["sis/azure" "https://portal.azure.com/?feature.msaljs=true#browse/Microsoft.App%2FcontainerApps"]
-            ["sis/timereport" "https://my.kleer.se/web2/time-reporting/month"]
-            ["sis/viewer" "https://dev-viewer.standard.sis.se"]
-            ["sis/viewer/test" "https://viewer-tst.standard.sis.se"]
-            ["sis/viewer/swagger" "https://api-dev.standard.sis.se/swagger/index.html"]
-            ["sis/mol" "https://mol-dev.sis.se"]
-            ["sis/mol-admin" "https://mol-admin-dev.sis.se"]
-          ];
-          caesari = [
-            ["caesari/gh" "https://github.com/caesariab/caesari2/pulls"]
-          ];
-          other = [
-            ["sports" "http://www.fawanews.sc"]
-          ];
-        };
-        toLine = pair: "${builtins.elemAt pair 1} ${builtins.elemAt pair 0}";
-        all = builtins.concatLists (builtins.attrValues bookmarks);
-      in
-        builtins.concatStringsSep "\n" (map toLine all);
 
       # Greasemonkey
       xdg.configFile."qutebrowser/greasemonkey/youtube_adblock.js".source = ./youtube_adblock.js;
