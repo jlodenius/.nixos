1b1918cfde38ff54bef57d76d3f320c142998c98
jacob <jacoblodenius@gmail.com>
2026-04-05 12:47:27 +0200

    Host thinkpad | Gen 218: 2026-04-05 12:47:27

---
+++ modules/qutebrowser/_bookmarks.nix
@@ -0,0 +1,40 @@
+let
+  toLine = pair: "${builtins.elemAt pair 1} ${builtins.elemAt pair 0}";
+
+  bookmarks = {
+    sis = [
+      ["sis/repos" "https://dev.azure.com/swedishinstituteforstandards/_git/sis-frontend"]
+      ["sis/teams" "https://teams.cloud.microsoft"]
+      ["sis/time" "https://my355752-sso.sapbydesign.com/sap/ap/ui/repository/SAP_UI/HTMLOBERON5/client.html"]
+      ["sis/jira" "https://sisswe.atlassian.net/jira/software/c/projects/SU/boards/24/backlog"]
+      ["sis/azure" "https://portal.azure.com/?feature.msaljs=true#browse/Microsoft.App%2FcontainerApps"]
+      ["sis/timereport" "https://my.kleer.se/web2/time-reporting/month"]
+      ["sis/viewer" "https://dev-viewer.standard.sis.se"]
+      ["sis/viewer/test" "https://viewer-tst.standard.sis.se"]
+      ["sis/viewer/swagger" "https://api-dev.standard.sis.se/swagger/index.html"]
+      ["sis/mol" "https://mol-dev.sis.se"]
+      ["sis/mol-admin" "https://mol-admin-dev.sis.se"]
+    ];
+    caesari = [
+      ["caesari/gh" "https://github.com/caesariab/caesari2/pulls"]
+    ];
+    other = [
+      ["sports" "http://www.fawanews.sc"]
+      ["fantasy" "https://fantasy.allsvenskan.se/my-team"]
+    ];
+  };
+in {
+  quickmarks = {
+    yt = "https://www.youtube.com";
+    z = "https://mail.zoho.eu/zm/#mail/folder/inbox";
+    gm = "https://mail.google.com/mail/u/0/#inbox";
+    gh = "https://github.com/jlodenius";
+    maps = "https://www.google.com/maps";
+    gemini = "https://gemini.google.com";
+    rd = "https://www.reddit.com";
+    ch = "https://www.chess.com";
+  };
+
+  bookmarks =
+    builtins.concatStringsSep "\n" (map toLine (builtins.concatLists (builtins.attrValues bookmarks)));
+}
--- modules/qutebrowser/qutebrowser.nix
+++ modules/qutebrowser/qutebrowser.nix
@@ -1,21 +1,13 @@
 {...}: {
   flake.nixosModules.qutebrowser = {config, ...}: let
     c = config.colours;
+    bm = import ./_bookmarks.nix;
   in {
     home-manager.users.jacob = {
       programs.qutebrowser = {
         enable = true;
 
-        quickmarks = {
-          yt = "https://www.youtube.com";
-          z = "https://mail.zoho.eu/zm/#mail/folder/inbox";
-          gm = "https://mail.google.com/mail/u/0/#inbox";
-          gh = "https://github.com/jlodenius";
-          maps = "https://www.google.com/maps";
-          gemini = "https://gemini.google.com";
-          rd = "https://www.reddit.com";
-          ch = "https://www.chess.com";
-        };
+        quickmarks = bm.quickmarks;
 
         keyBindings = {
           normal = {
@@ -42,7 +34,8 @@
           };
         };
 
-        # This is necessary because this home-manager module's tabs.padding options are buggy
+        # Note:
+        # This extraConfig is necessary because the tabs.padding option is buggy in this home-manager module
         extraConfig = ''
           c.tabs.padding = {'top': 6, 'bottom': 6, 'left': 6, 'right': 6}
           c.tabs.indicator.padding = {'top': 4, 'bottom': 4, 'left': 4, 'right': 4}
@@ -186,32 +179,7 @@
       };
 
       # Bookmarks
-      xdg.configFile."qutebrowser/bookmarks/urls".text = let
-        bookmarks = {
-          sis = [
-            ["sis/repos" "https://dev.azure.com/swedishinstituteforstandards/_git/sis-frontend"]
-            ["sis/teams" "https://teams.cloud.microsoft"]
-            ["sis/time" "https://my355752-sso.sapbydesign.com/sap/ap/ui/repository/SAP_UI/HTMLOBERON5/client.html"]
-            ["sis/jira" "https://sisswe.atlassian.net/jira/software/c/projects/SU/boards/24/backlog"]
-            ["sis/azure" "https://portal.azure.com/?feature.msaljs=true#browse/Microsoft.App%2FcontainerApps"]
-            ["sis/timereport" "https://my.kleer.se/web2/time-reporting/month"]
-            ["sis/viewer" "https://dev-viewer.standard.sis.se"]
-            ["sis/viewer/test" "https://viewer-tst.standard.sis.se"]
-            ["sis/viewer/swagger" "https://api-dev.standard.sis.se/swagger/index.html"]
-            ["sis/mol" "https://mol-dev.sis.se"]
-            ["sis/mol-admin" "https://mol-admin-dev.sis.se"]
-          ];
-          caesari = [
-            ["caesari/gh" "https://github.com/caesariab/caesari2/pulls"]
-          ];
-          other = [
-            ["sports" "http://www.fawanews.sc"]
-          ];
-        };
-        toLine = pair: "${builtins.elemAt pair 1} ${builtins.elemAt pair 0}";
-        all = builtins.concatLists (builtins.attrValues bookmarks);
-      in
-        builtins.concatStringsSep "\n" (map toLine all);
+      xdg.configFile."qutebrowser/bookmarks/urls".text = bm.bookmarks;
 
       # Greasemonkey
       xdg.configFile."qutebrowser/greasemonkey/youtube_adblock.js".source = ./youtube_adblock.js;
