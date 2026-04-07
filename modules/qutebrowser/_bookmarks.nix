1431fb5c2ffc3d3575001044db0dd54dd60d695f
jacob <jacoblodenius@gmail.com>
2026-04-07 14:32:10 +0200

    Host thinkpad | Gen 224: 2026-04-07 14:32:10

--- modules/qutebrowser/_bookmarks.nix
+++ modules/qutebrowser/_bookmarks.nix
@@ -1,28 +1,37 @@
 let
   toLine = pair: "${builtins.elemAt pair 1} ${builtins.elemAt pair 0}";
 
+  expand = name: value:
+    if builtins.isList value
+    then map (pair: ["${name}/${builtins.elemAt pair 0}" (builtins.elemAt pair 1)]) value
+    else [[name value]];
+
   bookmarks = {
     sis = [
-      ["sis/repos" "https://dev.azure.com/swedishinstituteforstandards/_git/sis-frontend"]
-      ["sis/teams" "https://teams.cloud.microsoft"]
-      ["sis/time" "https://my355752-sso.sapbydesign.com/sap/ap/ui/repository/SAP_UI/HTMLOBERON5/client.html"]
-      ["sis/jira" "https://sisswe.atlassian.net/jira/software/c/projects/SU/boards/24/backlog"]
-      ["sis/azure" "https://portal.azure.com/?feature.msaljs=true#browse/Microsoft.App%2FcontainerApps"]
-      ["sis/timereport" "https://my.kleer.se/web2/time-reporting/month"]
-      ["sis/viewer" "https://dev-viewer.standard.sis.se"]
-      ["sis/viewer/test" "https://viewer-tst.standard.sis.se"]
-      ["sis/viewer/swagger" "https://api-dev.standard.sis.se/swagger/index.html"]
-      ["sis/mol" "https://mol-dev.sis.se"]
-      ["sis/mol-admin" "https://mol-admin-dev.sis.se"]
+      ["repos" "https://dev.azure.com/swedishinstituteforstandards/_git/sis-frontend"]
+      ["teams" "https://teams.cloud.microsoft"]
+      ["time" "https://my355752-sso.sapbydesign.com/sap/ap/ui/repository/SAP_UI/HTMLOBERON5/client.html"]
+      ["jira" "https://sisswe.atlassian.net/jira/software/c/projects/SU/boards/24/backlog"]
+      ["azure" "https://portal.azure.com/?feature.msaljs=true#browse/Microsoft.App%2FcontainerApps"]
+      ["timereport" "https://my.kleer.se/web2/time-reporting/month"]
+      ["viewer" "https://dev-viewer.standard.sis.se"]
+      ["viewer/test" "https://viewer-tst.standard.sis.se"]
+      ["viewer/swagger" "https://api-dev.standard.sis.se/swagger/index.html"]
+      ["mol" "https://mol-dev.sis.se"]
+      ["mol-admin" "https://mol-admin-dev.sis.se"]
+      ["sd-local" "https://sd-api.dev.sis.se:7138/swagger/index.html"]
     ];
     caesari = [
-      ["caesari/gh" "https://github.com/caesariab/caesari2/pulls"]
+      ["gh" "https://github.com/caesariab/caesari2/pulls"]
     ];
-    other = [
-      ["sports" "http://www.fawanews.sc"]
-      ["fantasy" "https://fantasy.allsvenskan.se/my-team"]
-    ];
+
+    sports = "http://www.fawanews.sc";
+    fantasy = "https://fantasy.allsvenskan.se/my-team";
   };
+
+  prefixed = builtins.concatLists (
+    builtins.attrValues (builtins.mapAttrs expand bookmarks)
+  );
 in {
   quickmarks = {
     yt = "https://www.youtube.com";
@@ -35,6 +44,5 @@ in {
     ch = "https://www.chess.com";
   };
 
-  bookmarks =
-    builtins.concatStringsSep "\n" (map toLine (builtins.concatLists (builtins.attrValues bookmarks)));
+  bookmarks = builtins.concatStringsSep "\n" (map toLine prefixed);
 }
