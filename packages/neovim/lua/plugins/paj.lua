e4cb7b9e10dce082a0d9077c9594a85825e109a5
jacob <jacoblodenius@gmail.com>
2026-07-30 16:35:15 +0200

    Configure Paj output split

--- packages/neovim/lua/plugins/paj.lua
+++ packages/neovim/lua/plugins/paj.lua
@@ -1,5 +1,11 @@
 return {
   "paj.nvim",
   lazy = false,
-  after = function() vim.keymap.set("x", "<leader>ae", ":PajQuery<CR>", { desc = "Query Paj about selection" }) end,
+  after = function()
+    require("paj").setup({
+      output_size = 30,
+      output_position = "bottom",
+    })
+    vim.keymap.set("x", "<leader>ae", ":PajQuery<CR>", { desc = "Query Paj about selection" })
+  end,
 }
