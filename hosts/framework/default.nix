420383136446791ed378e0bd1343e5ef49e9c4f6
jlodenius <jacoblodenius@gmail.com>
2026-09-18 21:08:57 +0200

    Host framework | Gen 30: 2026-09-18 21:08:57

--- hosts/framework/default.nix
+++ hosts/framework/default.nix
@@ -63,6 +63,7 @@ in {
           "modesetting"
           "fbdev"
         ];
+        boot.extraModprobeConfig = "options iwlwifi disable_11be=1"; # Temporarily disable Wi-Fi 7 due to connection interruptions on the Deco mesh.
         boot.loader.systemd-boot.enable = true;
         boot.loader.efi.canTouchEfiVariables = true;
         networking.firewall.enable = false;
